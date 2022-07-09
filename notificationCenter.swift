e9813426e89141b58146c636cd9a2eb0b75d94f9
Sampel65 <sampeldan65@gmail.com>
2022-07-09 05:10:35 +0100

    second commit

---
+++ notificationCenter.swift
@@ -0,0 +1,20 @@
+//
+//  notificationCenter.swift
+//  todoList
+//
+//  Created by Sampel on 20/06/2022.
+//
+
+import SwiftUI
+
+struct notificationCenter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct notificationCenter_Previews: PreviewProvider {
+    static var previews: some View {
+        notificationCenter()
+    }
+}
